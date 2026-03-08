08a382fe0f318ac030d8172cbee325eef119dc9e
Sachin Chaurasiya <sachinchau444@gmail.com>
2026-03-08 16:39:47 +0530

    Testig bug

--- src/main/java/com/practice/security/SecurityApplication.java
+++ src/main/java/com/practice/security/SecurityApplication.java
@@ -1,11 +1,4 @@
 package com.practice.security;
-
-import java.security.NoSuchAlgorithmException;
-import java.util.Base64;
-
-import javax.crypto.KeyGenerator;
-import javax.crypto.SecretKey;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -14,6 +7,5 @@ public class SecurityApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SecurityApplication.class, args);
-		
 	}
 }
